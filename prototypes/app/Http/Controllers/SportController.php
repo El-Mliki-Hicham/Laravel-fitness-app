496db6433deee9edddc6042b71b34c195aee3c99
EL.MLIKI.HICHAM <92031524+El-Mliki-Hicham@users.noreply.github.com>
2022-05-20 21:14:53 +0100

    crud jours

--- prototypes/app/Http/Controllers/SportController.php
+++ prototypes/app/Http/Controllers/SportController.php
@@ -6,17 +6,17 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 class SportController extends Controller
 {
-    // public function  index(){
+//     public function  index(){
 
-    //     $users = DB::table('jours')
-    //     ->join('categories', 'jours.id_categories', '=', 'categories.id')
-    //     ->join('exercices', 'jours.id_exercice', '=', 'exercices.id')
-    //     ->select('*')
-    //     ->get()->toArray();  
+//         $users = DB::table('jours')
+//         ->join('categories', 'jours.id_categories', '=', 'categories.id')
+//         ->join('exercices', 'jours.id_exercice', '=', 'exercices.id')
+//         ->select('*')
+//         ->get()->toArray();  
         
         
     
-    //     return view('pages.index',compact('users')) ;
+//         return view('pages.index',compact('users')) ;
     
 // }
 public function index()
