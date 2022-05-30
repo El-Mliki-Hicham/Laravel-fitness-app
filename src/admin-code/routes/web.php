8f6781f32b15c24ee150dfd545cbc378338b037a
hicham <ELmliki.hicham@gmail.com>
2022-05-30 08:07:58 -0700

    cration des routes

--- src/admin-code/routes/web.php
+++ src/admin-code/routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\CategoriesController;
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\ExercicesController;
+use App\Http\Controllers\JoursController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +20,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+route::resource("/admin",Controller::class);
+route::resource("/admin-categorie",CategoriesController::class);
+route::resource("/admin-exercice",ExercicesController::class);
+route::resource("/admin-jour",JoursController::class);
