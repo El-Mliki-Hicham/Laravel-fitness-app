d03016f865ed957886820197344725054ec545cf
nada stitou <nadastito1087@gmail.com>
2022-05-30 23:20:34 +0100

    edit java

--- src/admin-code/resources/views/admin/index.blade.php
+++ src/admin-code/resources/views/admin/index.blade.php
@@ -348,13 +348,8 @@
   <!-- end content -->
 </section>
 <!-- end admin -->
-<!-- start screpting -->
-<script src="js/jquery.min.js"></script>
-<script src="js/tether.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script src="js/highcharts.js"></script>
-<script src="js/chart.js"></script>
-<script src="js/app.js"></script>
-<!-- end screpting -->
+
 </body>
+@include("layouts.footer")
 </html>
+
---
+++ src/admin-code/resources/views/layouts/footer.blade.php
@@ -0,0 +1,10 @@
+
+<!-- start screpting -->
+<script src="{{url('js/jquery.min.js')}}"></script>
+<script src="{{url('js/tether.min.js')}}"></script>
+<script src="{{url('js/bootstrap.min.js')}}"></script>
+<script src="{{url('js/highcharts.js')}}"></script>
+<script src="{{url('js/chart.js')}}"></script>
+<script src="{{url('js/app.js')}}"></script>
+<!-- end screpting -->
+<footer>@yield ('footer')</footer>
