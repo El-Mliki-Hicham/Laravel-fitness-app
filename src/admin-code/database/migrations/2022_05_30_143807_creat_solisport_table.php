0e60255f8049a9fc172f62d8a066eda4fc6761a9
hicham <ELmliki.hicham@gmail.com>
2022-05-30 08:01:33 -0700

    table migration

--- src/admin-code/database/migrations/2022_05_30_143807_creat_solisport_table.php
+++ src/admin-code/database/migrations/2022_05_30_143807_creat_solisport_table.php
@@ -13,7 +13,38 @@ return new class extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('categories', function (Blueprint $table) {
+            $table->increments("id_categorie");
+            $table->string('name_categorie')->nullable();
+            $table->string('description_categorie')->nullable();
+            $table->timestamps();
+
+        });
+        Schema::create('exercices', function (Blueprint $table) {
+            $table->increments("id_exercice")->nullable();
+            $table->string('nom_exercice')->nullable();
+            $table->string('numero')->nullable();
+            $table->string('description_exercice')->nullable();
+            $table->string('photo')->nullable();
+            $table->timestamps();
+
+        });
+
+        Schema::create('jours', function (Blueprint $table) {
+            $table->increments("id_jour")->nullable();
+            $table->string('jour')->nullable();
+            $table->unsignedInteger("categorie_id")->nullable();
+            $table->unsignedInteger("exercice_id")->nullable();
+            $table->timestamps();
+            $table->foreign('categorie_id')
+            ->references('id_categorie')
+            ->on('categories')
+            ->onDelete('cascade');;
+            $table->foreign('exercice_id')
+            ->references('id_exercice')
+            ->on('exercices')
+            ->onDelete('cascade');;
+        });
     }
 
     /**
