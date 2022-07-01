d6765cde51e692b50d7cc5fe38cb3eeb913eaae7
EL.MLIKI.HICHAM <92031524+El-Mliki-Hicham@users.noreply.github.com>
2022-07-01 16:37:25 +0200

    prototype checkbox

---
+++ prototype-5/app/Http/Controllers/checkboxController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\categories;
+use App\Models\categories_exerices;
+use App\Models\exercices;
+use App\Models\jours;
+use App\Models\exercices_de_jours;
+use Illuminate\Http\Request;
+
+class checkboxController extends Controller
+{
+    public function index(){
+        $jours = jours::all();
+        $exercices = exercices::select("*")->take(5)
+        ->join("categories_exerices","exercices.categorie_exercice",'=',"categories_exerices.id_categorie_exercice")
+      ->get();
+
+    return view("pages.index",compact('exercices',"jours"));
+    }
+
+
+
+public function ajouter(Request $request){
+
+
+$jours = $request->input("jour");
+$exercice = $request->input("exercice");
+
+exercices_de_jours::create([
+
+    
+])
+
+
+
+
+}
+
+
+
+
+
+
+
+
+}
+
+
